10eddbf75624d079f1116cd5745a30843577117d
Hao Nghiem <qtvhao@gmail.com>
2025-05-15 19:45:03 +0700

    Add

--- src/SystemClock/Domain/Contracts/FiveMinuteTickPublisherContract.ts
+++
@@ -1,7 +0,0 @@
-// src/SystemClock/Domain/Contracts/FiveMinuteTickPublisherContract.ts
-
-import { FiveMinuteTickOccurredEvent } from '../Events/FiveMinuteTickOccurredEvent';
-
-export interface FiveMinuteTickPublisherContract {
-  publish(event: FiveMinuteTickOccurredEvent): Promise<void>;
-}
--- src/SystemClock/Infrastructure/Scheduling/FiveMinuteTickSchedulerService.ts
+++
@@ -1,5 +0,0 @@
-// src/SharedKernel/Application/CommandBus.ts
-
-export interface CommandBus {
-  dispatch<TCommand>(command: TCommand): Promise<void>;
-}
